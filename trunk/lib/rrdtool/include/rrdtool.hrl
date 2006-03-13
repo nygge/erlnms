e0f16abfb718c43c7520137aa3fd2a6a1cb928a5
nygge <anders.nygren@gmail.com>
2006-03-13 23:00:45 +0000

    rrdtool: Added records for xport.

--- trunk/lib/rrdtool/include/rrdtool.hrl
+++ trunk/lib/rrdtool/include/rrdtool.hrl
@@ -6,56 +6,106 @@
 %%% Created :  5 Feb 2004 by Anders Nygren <anders.nygren@gmail.com>
 %%%-------------------------------------------------------------------
 
-
-%%------------------------------------------------
-%%
-%% create(Port,File,Opts,DSs,RRAs) -> {ok,nothing} | {error,Reason}
-%%
-%% FileName    = string, name of the file
-%% Opts        = [Opt]
-%% Opt         = {start,DateTime} | {step, Duration}
-%% DateTime    = {{YYYY,MM,DD},{HH,MM,SS}}
 %% Duration    = {Unit,Integer}
 %% Unit        = sec | min | hour | day | week | month | year
 
--record(rrd_file,{file,start,step,dss,rras,last}).
+-record(rrd_file,{file,   % string, name of the file
+		  start,  % Start of file
+		  step,   % Duration
+		  dss,    % [rrd_dss]
+		  rras,   % [rrd_rra]
+		  last    % Timestamp of last update
+		 }).
 
-%%------------------------------------------------
-%% Data Source definition
-%%
-%% DS = {Name,DST,HB,Min,Max}
-%% Name        = Data Source Name, 
-%%             = string | atom
-%% DST         = Data Source Type
-%%             = 'GAUGE' | 'COUNTER' | 'DERIVE' | 'ABSOLUTE'
-%% HB          = Duration, Max number of seconds between two updates
-%% Min         = integer, Min allowed value
-%% Max         = integer, Max allowed value
+-record(rrd_ds,{name,       % Data Source Name, 
+		type,       % 'GAUGE' | 'COUNTER' | 'DERIVE' | 'ABSOLUTE'
+		hb,         % Max number of seconds between two updates
+		min,        % integer, Min allowed value
+		max,        % integer, Max allowed value
+		last,       % Not for create command
+		value,      % Not for create command
+		unknown_sec % Not for create command
+	       }).
 
--record(rrd_ds,{name,type,hb,min,max,last,value,unknown_sec}).
-
--record(rrd_ds_comp,{name,rpn}).
+-record(rrd_ds_comp,{name,  % Data Source Name, 
+		     rpn    % RPN expression for calculating DS value
+		    }).
 
 %%------------------------------------------------
 %% Round Robin Archive definition
 %%
 %% RRA = {'CF',XFF,ConsInt,ArchiveTime}
 %% CF          = Consolidation Function
-%%             = 'AVERAGE' | 'MIN' | 'MAX' | 'LAST'
-%% XFF         = X-Files Factor, 0<XFF<1
-%% ConsInt     = {Unit,No}, Consolidation Interval
-%% ArchiveTime = {Unit,No}, Time duration that the archive contains
+%%             = 
+%% XFF         = 
+%% ConsInt     = 
+%% ArchiveTime = 
 %% Unit        = sec | min | hour | day | week | month | year
 %% No          = integer
 
--record(rrd_rra, {id,cf,xff,interval,duration}).
+-record(rrd_rra, {id,       % 
+		  cf,       % 'AVERAGE' | 'MIN' | 'MAX' | 'LAST'
+		  xff,      % X-Files Factor, 0<XFF<1
+		  interval, % {Unit,No}, Consolidation Interval
+		  duration  % {Unit,No}, Duration the archive contains
+		 }).
 
--record(rrd_rra_hwpred,{id,rows,alpha,beta,period,rra_num}).
+-record(rrd_rra_hwpred,{id,
+			rows,
+			alpha,
+			beta,
+			period,
+			rra_num
+		       }).
 
--record(rrd_rra_seasonal,{id,period,gamma,rra_num}).
+-record(rrd_rra_seasonal,{id,
+			  period,
+			  gamma,
+			  rra_num
+			 }).
 
--record(rrd_rra_devseason,{id,period,gamma,rra_num}).
+-record(rrd_rra_devseason,{id,
+			   period,
+			   gamma,
+			   rra_num
+			  }).
 
--record(rrd_rra_devpredict,{id,rows,rra_num}).
+-record(rrd_rra_devpredict,{id,
+			    rows,
+			    rra_num
+			   }).
 
--record(rrd_rra_failures,{id,rows,threshold,window,rra_num}).
+-record(rrd_rra_failures,{id,
+			  rows,
+			  threshold,
+			  window,
+			  rra_num
+			 }).
+
+
+%%------------------------------------------------
+%% records for graph and export
+%%
+
+-record(rrd_export,{start,   % Start time
+		    stop,    % End time
+		    rows,    % Max number of rows
+		    step,    % Data point step
+		    defs,    % [rrd_def]
+		    cdefs,   % [rrd_cdef]
+		    xports   % [rrd_xport]
+		   }).
+
+-record(rrd_def,{vname,      % Variable name
+		 rrd,        % RRD filename
+		 ds_name,    % Datasource name
+		 cf          % Consolidation functions
+		}).
+
+-record(rrd_cdef,{vname,     % Variable name
+		  rpn        % RPN expression
+		 }).
+
+-record(rrd_xport,{vname,    % Variable name
+		   legend    % Exported name
+		  }).
