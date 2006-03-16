c7c7bf24463e6da2ab8bf2a74bedee222a39eb26
nygge <anders.nygren@gmail.com>
2006-03-16 22:50:30 +0000

    pm_basic,pm_rrdtool: Moved database tables.

--- trunk/lib/pm_basic/src/pm_basic_mk_tabs.erl
+++ trunk/lib/pm_basic/src/pm_basic_mk_tabs.erl
@@ -8,23 +8,48 @@
 -module(pm_basic_mk_tabs).
 
 -export([all/0,all/1,
-	 pm_db_backend/1,
+	 pm_store_inst/1,
+         pm_store_type/1,
+         pm_archive/1,
+         pm_aggregate/1,
+         pm_mo_type/1,
+         pm_counter/1,
+         pm_der_counter/1,
 	 pm_duration/1,
 	 pm_event/1
 	]).
 
--include("pm_config.hrl").
+-include("pm_store.hrl").
 
 all() ->
     all([node()]).
 
 all(Nodes) ->
-    pm_db_backend(Nodes),
+    pm_store_inst(Nodes),
+    pm_store_type(Nodes),
+    pm_archive(Nodes),
+    pm_aggregate(Nodes),
+    pm_mo_type(Nodes),
+    pm_counter(Nodes),
+    pm_der_counter(Nodes),
     pm_duration(Nodes),
     pm_event(Nodes).
 
-pm_db_backend(Nodes) ->
-    mk_tab(Nodes,pm_db_backend,record_info(fields,pm_db_backend)).
+
+pm_store_inst(Nodes) ->
+    mk_tab(Nodes,pm_store_inst,record_info(fields,pm_store_inst)).
+pm_store_type(Nodes) ->
+    mk_tab(Nodes,pm_store_type,record_info(fields,pm_store_type)).
+pm_archive(Nodes) ->
+    mk_tab(Nodes,pm_archive,record_info(fields,pm_archive)).
+pm_aggregate(Nodes) ->
+    mk_tab(Nodes,pm_aggregate,record_info(fields,pm_aggregate)).
+pm_mo_type(Nodes) ->
+    mk_tab(Nodes,pm_mo_type,record_info(fields,pm_mo_type)).
+pm_counter(Nodes) ->
+    mk_tab(Nodes,pm_counter,record_info(fields,pm_counter)).
+pm_der_counter(Nodes) ->
+    mk_tab(Nodes,pm_der_counter,record_info(fields,pm_der_counter));
 pm_duration(Nodes) ->
     mk_tab(Nodes,pm_duration,record_info(fields,pm_duration)).
 pm_event(Nodes) ->
--- trunk/lib/pm_rrdtool/src/pm_rrdtool_mkTabs.erl
+++ trunk/lib/pm_rrdtool/src/pm_rrdtool_mkTabs.erl
@@ -8,13 +8,7 @@
 -module(pm_rrdtool_mkTabs).
 
 -export([all/0,all/1,
-	 pm_store_inst/1,
-	 pm_store_type/1,
-	 pm_archive/1,
-	 pm_aggregate/1,
-	 pm_mo_type/1,
-	 pm_counter/1,
-	 pm_der_counter/1
+	 pm_rrd_inst/1
 	]).
 
 -include("pm_rrdtool.hrl").
@@ -23,28 +17,10 @@ all() ->
     all([node()]).
 
 all(Nodes) ->
-    pm_store_inst(Nodes),
-    pm_store_type(Nodes),
-    pm_archive(Nodes),
-    pm_aggregate(Nodes),
-    pm_mo_type(Nodes),
-    pm_counter(Nodes),
-    pm_der_counter(Nodes).
+    pm_rrd_inst(Nodes).
 
-pm_store_inst(Nodes) ->
-    mk_tab(Nodes,pm_store_inst,record_info(fields,pm_store_inst)).
-pm_store_type(Nodes) ->
-    mk_tab(Nodes,pm_store_type,record_info(fields,pm_store_type)).
-pm_archive(Nodes) ->
-    mk_tab(Nodes,pm_archive,record_info(fields,pm_archive)).
-pm_aggregate(Nodes) ->
-    mk_tab(Nodes,pm_aggregate,record_info(fields,pm_aggregate)).
-pm_mo_type(Nodes) ->
-    mk_tab(Nodes,pm_mo_type,record_info(fields,pm_mo_type)).
-pm_counter(Nodes) ->
-    mk_tab(Nodes,pm_counter,record_info(fields,pm_counter)).
-pm_der_counter(Nodes) ->
-    mk_tab(Nodes,pm_der_counter,record_info(fields,pm_der_counter)).
+pm_rrd_inst(Nodes) ->
+    mk_tab(Nodes,pm_rrd_inst,record_info(fields,pm_rrd_inst)).
     
 mk_tab(Nodes,Tab,Fields) ->
     mnesia:create_table(Tab,
