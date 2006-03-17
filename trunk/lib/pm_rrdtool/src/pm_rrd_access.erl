6a4a7eb36cb52aac97bcaf1c98007ac7d9db1e0e
nygge <anders.nygren@gmail.com>
2006-03-17 00:23:42 +0000

    pm_rrdtool: Fixed recordtype.

--- trunk/lib/pm_rrdtool/src/pm_rrd_access.erl
+++ trunk/lib/pm_rrdtool/src/pm_rrd_access.erl
@@ -12,7 +12,7 @@
 %%--------------------------------------------------------------------
 -include("pm_rec.hrl").
 -include("pm_store.hrl").
-%%-include("pm_rrdtool.hrl").
+-include("pm_rrdtool.hrl").
 
 %%--------------------------------------------------------------------
 %% External exports
@@ -42,9 +42,8 @@ create(Name,MOI,Store_type) when is_atom(name),is_list(MOI),is_atom(Store_type)-
     MO_TYPE=ST#pm_store_type.mo_type,
     File=mk_file_name(Name,MOI,MO_TYPE),
     {ok,nothing}=rrdtool:create(File,[Step],DSS,RRA),
-    STORE_INST=#pm_store_inst{name={MOI,MO_TYPE},store_type=Store_type,
-			     file=File},
-    {atomic,_Reply}=pm_rrd_config:insert(STORE_INST).
+    {atomic,_Reply}=pm_rrd_config:insert(#pm_rrd_inst{name={MOI,MO_TYPE},
+						      file=File}).
 
 %% @spec fetch(MOI,MOC,Counters,CF,Res,Start,Stop) -> Result
 %% MOI         = [RDN]
