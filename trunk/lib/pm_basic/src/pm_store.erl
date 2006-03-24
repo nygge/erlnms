c51ba7535db8e514c71259bf34cdbc09fe18a75a
nygge <anders.nygren@gmail.com>
2006-03-24 17:12:19 +0000

    Changed to use db backends.

--- trunk/lib/pm_basic/src/pm_store.erl
+++ trunk/lib/pm_basic/src/pm_store.erl
@@ -12,11 +12,12 @@
 %%--------------------------------------------------------------------
 
 -include("pm_store.hrl").
+-include("new_pm_data.hrl").
 
 %%--------------------------------------------------------------------
 %% External exports
 -export([
-%	 create/3,
+	 create/3,
 % 	 fetch/1,
 %%	 fetch/4,
 	 fetch/5,
@@ -29,27 +30,34 @@
 %% External functions
 %%====================================================================
 
-%% %% @spec create(Name,MOI,Store_type,BackEnd) -> Result
-%% %% Name        = atom()
-%% %% MOI         = [RDN]
-%% %% RDN         = {Type,Id}
-%% %% Type        = atom()
-%% %% Id          = atom()
-%% %% Store_type  = atom()
-%% %% BackEnd     = atom()
-%% %% Result      = {atomic,Reply}
-%% %% Reply       = WHAT
-%% %% @doc Create a measurement store.
+%% @spec create(Name,MOI,Store_type,BackEnd) -> Result
+%% Name        = atom()
+%% MOI         = [RDN]
+%% RDN         = {Type,Id}
+%% Type        = atom()
+%% Id          = atom()
+%% Store_type  = atom()
+%% BackEnd     = atom()
+%% Result      = {atomic,Reply}
+%% Reply       = WHAT
+%% @doc Create a measurement store.
 
-%% create(Name,MOI,Store_type,BackEnd) when is_atom(name),is_list(MOI),is_atom(Store_type)->
-%%     {Step,DSS,RRA}=get_store_type_def(Store_type),
-%%     [ST]=pm_config:get(pm_store_type,Store_type),
-%%     MO_TYPE=ST#pm_store_type.mo_type,
-%%     File=mk_file_name(Name,MOI,MO_TYPE),
-%%     {ok,nothing}=rrdtool:create(File,[Step],DSS,RRA),
-%%     STORE_INST=#pm_store_inst{name={MOI,MO_TYPE},store_type=Store_type,
-%% 			     file=File},
-%%     {atomic,Reply}=pm_config:insert(STORE_INST).
+create(MOI,StoreType,BackEnd) when is_list(MOI),
+				   is_atom(StoreType),
+				   is_atom(BackEnd) ->
+    ST=pm_config:get_store_def(StoreType),
+    BE=pm_config:get_db_backend(BackEnd),
+    MO_TYPE=(ST#pm_store_type.mo_type)#pm_mo_type.name,
+    case catch (BE#pm_db_backend.module):create(MOI,ST) of
+	{ok,nothing} ->
+	    {atomic,Reply}=pm_config:new_store_inst(
+			     #pm_store_inst{name={MOI,MO_TYPE},
+					    store_type=StoreType,
+					    backend=BackEnd}),
+	    ok;
+	Error ->
+	    Error
+    end.
 
 %% %% @spec fetch(MOI,MOC,Res) -> Result
 %% %% MOI         = [RDN]
@@ -141,7 +149,16 @@ fetch(MOI,MOC,Counters,CF,Res,Start,Stop) when is_tuple(Start) ->
 update(MOI,MOC,Time,Data) ->
     case pm_config:get_db_backend(MOI,MOC) of
 	{ok,Module} ->
-	    Module:update(MOI,MOC,Time,Data);
+	    Module:update(MOI,MOC,Time,Data),
+	    case pm_config:get_events(MOI,MOC) of
+		{found,Events,Step} ->
+		    S={Unit,No}=pm_config:get_duration(Step),
+		    lists:foreach(fun (EInt) ->
+					  send_event(MOI,MOC,EInt,check_time(Time,EInt,S))
+				  end,Events);
+		not_found ->
+		    ignore
+	    end;
 	_ ->
 	    {error,no_db_defined}
     end.
@@ -149,4 +166,20 @@ update(MOI,MOC,Time,Data) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+check_time(TS,EInt,Step) ->
+    Secs=calendar:datetime_to_gregorian_seconds(TS),
+    StepSecs=utils:duration_to_seconds(Step),
+    ESecs=utils:duration_to_seconds(EInt),
+    Latest=ESecs*(Secs div ESecs),
+    case (Secs-Latest)<StepSecs of
+	true ->
+	    {true,calendar:gregorian_seconds_to_datetime(Latest)};
+	false ->
+	    false
+    end.
+
+send_event(MOI,MOT,EInt,{true,Time}) ->
+    pm_data:send(#new_pm_data{moi=MOI,moc=MOT,int=EInt,time=Time});
+send_event(_MOI,_MOT,_EInt,false) ->
+    ok.
 
